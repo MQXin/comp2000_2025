535cb5e0880e53f442b30a725e9322fe517536ce
MQXin <Xin.Dobbie@gmail.com>
2025-08-18 10:19:13 +1000

    Made cell class use the rectangle object for it

--- src/Cell.java
+++ src/Cell.java
@@ -1,36 +1,28 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 
-public class Cell {
-  // fields
-  int x;
-  int y;
-  static int size = 35;
+public class Cell extends Rectangle {
+    // fields
+    int x;
+    int y;
+    static int size = 35;
 
-  // constructors
-  public Cell(int inX, int inY) {
-    x = inX;
-    y = inY;
-  }
-
-  // methods
-  public void paint(Graphics g, Point mousePos) {
-    if(contains(mousePos)) {
-      g.setColor(Color.GRAY);
-    } else {
-      g.setColor(Color.WHITE);
+    // constructors
+    public Cell(int inX, int inY) {
+        super(inX, inY, size, size);
     }
-    g.fillRect(x, y, size, size);
-    g.setColor(Color.BLACK);
-    g.drawRect(x, y, size, size);
-  }
 
-  public boolean contains(Point p) {
-    if(p != null) {
-      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
-    } else {
-      return false;
+    // methods
+    public void paint(Graphics g, Point mousePos) {
+        if (mousePos != null && this.contains(mousePos)) {
+            g.setColor(Color.GRAY);
+        } else {
+            g.setColor(Color.WHITE);
+        }
+        g.fillRect((int) this.getX(), (int) this.getY(), (int) this.getWidth(), (int) this.getHeight());
+        g.setColor(Color.BLACK);
+        g.drawRect((int) this.getX(), (int) this.getY(), (int) this.getWidth(), (int) this.getHeight());
     }
-  }
 }
